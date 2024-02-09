49ac30a8e16675b36c603db7c5b395186569bd52
Jason Jugarpython3 -u /home/noaa_gms/IFSS/IFSS.py <rfims.rfims@gmail.com>
2024-02-09 17:52:00 +0000

    add db push capabitilites

---
+++ IFSS_db.py
@@ -0,0 +1,125 @@
+from ctypes import *
+from time import sleep
+from RSA_API import *
+import sys
+from datetime import datetime
+from pymongo import MongoClient
+
+# MongoDB setup
+client = MongoClient('mongodb://localhost:27017/')
+db = client.ifss
+collection = db.spectrumData
+
+RTLD_LAZY = 0x0001
+LAZYLOAD = RTLD_LAZY | RTLD_GLOBAL
+rsa = CDLL("./libRSA_API.so",LAZYLOAD)
+usbapi = CDLL("./libcyusb_shared.so",LAZYLOAD)
+
+"""################CLASSES AND FUNCTIONS################"""
+def err_check(rs):
+    if ReturnStatus(rs) != ReturnStatus.noError:
+        raise RSAError(ReturnStatus(rs).name)
+
+def search_connect():
+    numFound = c_int(0)
+    intArray = c_int * DEVSRCH_MAX_NUM_DEVICES
+    deviceIDs = intArray()
+    deviceSerial = create_string_buffer(DEVSRCH_SERIAL_MAX_STRLEN)
+    deviceType = create_string_buffer(DEVSRCH_TYPE_MAX_STRLEN)
+    apiVersion = create_string_buffer(DEVINFO_MAX_STRLEN)
+
+    rsa.DEVICE_GetAPIVersion(apiVersion)
+    # print('API Version {}\n'.format(apiVersion.value.decode()))
+
+    err_check(rsa.DEVICE_Search(byref(numFound), deviceIDs,
+                                deviceSerial, deviceType))
+
+    if numFound.value < 1:
+        print('No instruments found. Exiting script.')
+        sys.exit(0)
+    elif numFound.value == 1:
+        err_check(rsa.DEVICE_Connect(deviceIDs[0]))
+    rsa.CONFIG_Preset()
+
+"""################SPECTRUM EXAMPLE################"""
+def config_spectrum(cf=1e9, refLevel=0, span=40e6, rbw=300e3):
+    rsa.SPECTRUM_SetEnable(c_bool(True))
+    rsa.CONFIG_SetCenterFreq(c_double(cf))
+    rsa.CONFIG_SetReferenceLevel(c_double(refLevel))
+
+    rsa.SPECTRUM_SetDefault()
+    specSet = Spectrum_Settings()
+    rsa.SPECTRUM_GetSettings(byref(specSet))
+    specSet.window = SpectrumWindows.SpectrumWindow_Kaiser
+    specSet.verticalUnit = SpectrumVerticalUnits.SpectrumVerticalUnit_dBm
+    specSet.span = span
+    specSet.rbw = rbw
+    rsa.SPECTRUM_SetSettings(specSet)
+    rsa.SPECTRUM_GetSettings(byref(specSet))
+    return specSet
+
+def acquire_spectrum(specSet):
+    ready = c_bool(False)
+    traceArray = c_float * specSet.traceLength
+    traceData = traceArray()
+    outTracePoints = c_int(0)
+    traceSelector = SpectrumTraces.SpectrumTrace1
+
+    rsa.DEVICE_Run()
+    rsa.SPECTRUM_AcquireTrace()
+    while not ready.value:
+        rsa.SPECTRUM_WaitForDataReady(c_int(100), byref(ready))
+    rsa.SPECTRUM_GetTrace(traceSelector, specSet.traceLength, byref(traceData), byref(outTracePoints))
+    rsa.DEVICE_Stop()
+    
+    # Convert C float array to Python list of floats...arggghhh
+    return [float(traceData[i]) for i in range(specSet.traceLength)]
+
+def spectrum_capture():
+    print('########CAPTURING....########')
+    search_connect()
+    
+    # Define center frequency, span, and RBW in MHz cause math...
+    cf_mhz = 2020
+    span_mhz = 20.0
+    rbw_khz = 15.0
+    refLevel = -80
+    
+    # Convert MHz to Hz for the API calls
+    cf_hz = cf_mhz * 1e6
+    span_hz = span_mhz * 1e6
+    rbw_hz = rbw_khz * 1e3
+    
+    specSet = config_spectrum(cf_hz, refLevel, span_hz, rbw_hz)
+    spec_settings = Spectrum_Settings()
+    err_check(rsa.SPECTRUM_GetSettings(byref(spec_settings)))
+    trace_length = spec_settings.traceLength
+    
+    # Adjust frequency list calculation for 10 frequency points using MHz values
+    startfreq_mhz = cf_mhz - (span_mhz / 2)
+    stopfreq_mhz = cf_mhz + (span_mhz / 2)
+    step_mhz = (stopfreq_mhz - startfreq_mhz) / (trace_length - 1)
+    freq_list_mhz = [startfreq_mhz + i * step_mhz for i in range(trace_length)]
+ 
+    cycle = 0
+    while cycle < 10: # Remove this later
+        cycle += 1
+        trace = acquire_spectrum(specSet)
+        currentTime = datetime.today().isoformat(sep=' ', timespec='milliseconds')
+
+        # Create a document for MongoDB and insert into thew collection
+        frequencies = {str(round(freq, 4)): float(value) for freq, value in zip(freq_list_mhz, trace)}
+        document = {
+            "timestamp": currentTime,
+            "frequencies": frequencies
+        }
+        collection.insert_one(document)
+
+        sleep(1) 
+        print(f'Last Trace Index: {cycle}')
+
+def main():
+    spectrum_capture()
+
+if __name__ == '__main__':
+    main()
