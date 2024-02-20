6161071e0b55a286e7f33e282b73e2477f66990e
guerillagorilla <novajeeper@gmail.com>
2024-02-20 14:08:04 +0000

    cleanup and fix schedule fetch

--- Dashboard/app.py
+++ Dashboard/app.py
@@ -6,45 +6,57 @@ from datetime import datetime, timedelta
 # Logging setup
 for handler in logging.root.handlers[:]:
     logging.root.removeHandler(handler)
-logging.basicConfig(filename='/home/noaa_gms/IFSS/IFSS.log', level=logging.INFO, format='%(asctime)s %(message)s', datefmt='%Y-%m-%d %H:%M:%S')
-logger = logging.getLogger(__name__)
+logging.basicConfig(filename='/home/noaa_gms/IFSS/IFSS_SA.log', level=logging.INFO, format='%(asctime)s %(message)s', datefmt='%Y-%m-%d %H:%M:%S')
 
 app = Flask(__name__)
 
 client = MongoClient("mongodb://localhost:27017/")
 db = client["ifss"]
-spectrumCollection = db["spectrumData"]
-scheduleCollection = db["satSchedule"]
+spectrumData = db["spectrumData"]
+satSchedule = db["satSchedule"]
 
 @app.route('/daily-schedule')
 def daily_schedule():
+    # print('Running daily-schedule')
     utc_now = datetime.utcnow()
     today_start = utc_now.replace(hour=0, minute=0, second=0, microsecond=0)
     today_end = today_start + timedelta(days=1)
 
-    schedules_today = scheduleCollection.find({
+    schedules_today = satSchedule.find({
         "timestamp": {
             "$gte": today_start,
             "$lt": today_end
         }
     })
+    query = {
+        "timestamp": {
+            "$gte": today_start,
+            "$lt": today_end
+        }
+    }
 
     schedules_list = []
-    for schedule in schedules_today:
-        schedule["_id"] = str(schedule["_id"])
-        schedules_list.append({
-            "item": schedule.get("item"),
-            "dir": schedule.get("dir"),
-            "el": schedule.get("el"),
-            "endDate": schedule.get("endDate"),
-            "endTime": schedule.get("endTime"),
-            "idle": schedule.get("idle"),
-            "mode": schedule.get("mode"),
-            "ovr": schedule.get("ovr"),
-            "sat": schedule.get("sat"),
-            "startDate": schedule.get("startDate"),
-            "startTime": schedule.get("startTime"),
-        })
+    for document in schedules_today:
+        document_id = str(document["_id"])
+        if "schedule" in document:
+            for schedule in document["schedule"]:
+                extracted_schedule = {
+                    "item": schedule.get("item"),
+                    "dir": schedule.get("dir"),
+                    "el": schedule.get("el"),
+                    "endDate": schedule.get("endDate"),
+                    "endTime": schedule.get("endTime"),
+                    "idle": schedule.get("idle"),
+                    "mode": schedule.get("mode"),
+                    "ovr": schedule.get("ovr"),
+                    "sat": schedule.get("sat"),
+                    "startDate": schedule.get("startDate"),
+                    "startTime": schedule.get("startTime"),
+                }
+                schedules_list.append(extracted_schedule)
+                # logging.info("Added schedule: %s", extracted_schedule)
+        else:
+            logging.info(f"Document {document_id} contains no schedule data.")
 
     return jsonify(schedules_list)
 
@@ -54,7 +66,7 @@ def index():
 
 @app.route('/data')
 def data():
-    document = spectrumCollection.find().sort([('_id', -1)]).limit(1)
+    document = spectrumData.find().sort([('_id', -1)]).limit(1)
     for doc in document:
         return jsonify(doc['frequencies'])
     return jsonify({})
